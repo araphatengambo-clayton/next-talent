3c75b6c71d843f544e9510ce69c5bf44e8531fea
araphatengambo-clayton <araphatengambo307@gmail.com>
2026-09-03 19:03:19 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next Talent",
+  description: "La plateforme qui révèle les talents du football amateur.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
